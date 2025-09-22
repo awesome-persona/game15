34d057d1c8bac6fa60d5ad6399232b5534ff717d
Andrey Narut <anarut@objectstyle.com>
2025-09-22 20:13:05 +0300

    Unit 2: replace fxml to java code

--- src/main/java/org/example/game/HelloApplication.java
+++ src/main/java/org/example/game/HelloApplication.java
@@ -1,18 +1,35 @@
 package org.example.game;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class HelloApplication extends Application {
 
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+    public void start(Stage stage) {
+
+        VBox vBox = new VBox();
+        vBox.setAlignment(Pos.CENTER);
+        vBox.setSpacing(20.0);
+
+        Insets insets = new Insets(20.0, 20.0, 20.0, 20.0);
+        vBox.setPadding(insets);
+
+        Label welcomeText = new Label();
+        vBox.getChildren().add(welcomeText);
+        Button button = new Button("Start");
+        button.setOnAction(actionEvent -> {
+            welcomeText.setText("Welcome to JavaFX Application!");
+        });
+        vBox.getChildren().add(button);
+
+        Scene scene = new Scene(vBox, 320, 240);
 
         stage.setTitle("Game 15");
         stage.setScene(scene);
